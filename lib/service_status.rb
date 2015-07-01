dc907e5615af137e9810768b97fc5e566481e474
Olga Scott <Olya@Olgas-MacBook-Pro.local>
2015-07-01 15:11:46 +0100

    pushed uncommited files

--- lib/service_status.rb
+++ lib/service_status.rb
@@ -1,5 +1,31 @@
 require "service_status/version"
+require "httparty"
 
 module ServiceStatus
-  # Your code goes here...
+  include HTTParty
+  base_uri 'localhost:3000/api/1'
+
+  def self.get_status
+    get("/reports/current_status").parsed_response
+  end
+
+  def self.get_reports
+    get("/reports").parsed_response
+  end
+
+  def self.signup(email, password)
+    body = {email: email, password: password}
+    post("/signup", body: body).parsed_response
+  end
+
+  def self.login(email, password)
+    body = {email: email, password: password}
+    post("/login", body: body).parsed_response
+  end
+
+  def self.create_report(access_token, status = nil, message = nil)
+    body = {status: status, message: message}
+    headers = {'Authorization' => access_token}
+    post("/reports", body: body, headers: headers).parsed_response
+  end
 end
